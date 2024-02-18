35480f866d75bdf26cf4dd4855fef2fe0d6c33e7
thisJuanDev <97639572+thisJuanDev@users.noreply.github.com>
2024-02-18 13:22:41 -0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// Containers
+const main = document.getElementById("main");
+// Scenes
+const home = document.getElementById("home");
+const gallery = document.getElementById("gallery");
+const contact = document.getElementById("contact");
+const links = document.getElementById("links");
+// Buttons
+const homeButton = document.getElementById("home-button");
+const galleryButton = document.getElementById("gallery-button");
+const contactButton = document.getElementById("contact-button");
+const linksButton = document.getElementById("links-button");
+
